f8e031a59b707a912a4cdb3b5ccce247f4fb2791
leor0104 <luisortiz141999@gmail.com>
2021-12-27 21:40:34 -0500

    Implement loading skeletons

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Insertar Texto"
       value={searchValue}
       onChange={onSearchValueChange}
     />
---
+++ src/TodosError/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodosError ({ error }){
+    return <p>Lo siento, hubo un error :(</p>
+}
+
+export { TodosError };
